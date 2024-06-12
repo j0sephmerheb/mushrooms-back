ef3e4fd297d01e76b72d6120102922ea362c6833
Joseph Merheb <joseph@merheb.net>
2024-06-12 22:17:34 +0200

    20240612 - enableCors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://mushrooms.josephmerheb.net',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: false, // Enable to send cookies or authentication headers
+  });
+
   await app.listen(3000);
 }
 bootstrap();
